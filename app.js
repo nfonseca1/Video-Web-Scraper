819759448c25ce5ef73109eccf20d4c3f77223dc
Nathan <nathan.fonseca001@gmail.com>
2020-08-16 03:57:00 -0400

    New features, better searching

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     let keywords = req.body.keyword.split("; ");
+    let options = {
+        resultsCount: req.body.count || 10,
+        website: req.body.website
+    }
 
     req.session.results = {
         finished: false,
@@ -27,8 +31,7 @@ app.post("/", (req, res) => {
         await (async () => {
             for(let keyword of keywords){
                 req.session.results.links.push([])
-                await scrape(keyword, req);
-                console.log(req.session.results.length);
+                await scrape(keyword, req, options);
             }
         })();
         req.session.results.finished = true;
@@ -45,11 +48,12 @@ app.get("/status", (req, res) => {
     }
 })
 
-let scrape = async (keyword, req) => {
+let scrape = async (keyword, req, options) => {
 
-    let url = `https://www.google.com/search?q=${keyword}&source=lnms&tbm=vid`;
+    let url = `https://www.google.com/search?q=${keyword}&source=lnms&tbm=vid&num=${options.resultsCount}&as_sitesearch=${options.website}`;
 
     const browser = await puppeteer.launch({
+        headless: false,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
@@ -66,27 +70,59 @@ let scrape = async (keyword, req) => {
 
     await (async () => {
         for(let link of links) {
+            console.log("searching: " + link);
             await page.goto(link, {waitUntil: "networkidle2"});
     
-            let src = await page.evaluate(async () => {
+            let srcArr = await page.evaluate(async () => {
                 return await new Promise(resolve => {
                     // Select video tag and get src (from either video tag, or child source tag)
-                    let video = document.querySelector("video");
-                    let srcAtt = video?.getAttribute("src") 
-                        || video?.querySelector("source")?.getAttribute("src");
+                    let videos = document.querySelectorAll("video");
+                    console.log(videos);
+                    let videoArr = [];
+                    let checkForSource = () => {
+                        for(let video of videos) {
+                            let srcAtt = video?.querySelector("source")?.getAttribute("src") 
+                                || video?.getAttribute("src");
+                            if (srcAtt) videoArr.push(srcAtt);
+                        }
+                    }
+                    checkForSource();
+                    if (videoArr.length === 0) {
+                        let playBtn = document?.querySelector(".videoplayer");
+                        if (playBtn) {
+                            playBtn.click();
+                            console.log(document.querySelectorAll("video"));
+                            checkForSource();
+                        }
+                    }
 
-                    resolve(srcAtt);
+                    resolve(videoArr);
                 })
             });
             // Filter src tag to make sure it's not undefined or a blob
-            if (src) {
+            console.log(srcArr);
+            for(let src of srcArr){
+                if (src[0] === "/") {
+                    if (src[1] !== "/" && src[1] !== "h") {
+                        let end = ['.com', '.org', '.net'];
+                        for (let i = 0; i < end.length; i++){
+                            if (link.includes(end[i])) {
+                                let website = link.split(end[i])[0] + end[i];
+                                src = website + src;
+                                break;
+                            }
+                        }
+                    }
+                    else if (src[1] === "/") src = "http:" + src;
+                }
                 let isBlob = src.includes("blob:") ? true : false;
-                if (!isBlob) {
+                let isData = src.includes("data:") ? true : false;
+                if (!isBlob && !isData) {
                     req.session.reload(() => {
                         let results = req.session.results.links;
                         results[results.length-1].push(src); // Add link to last session array
                         req.session.save(() => {
-                            console.log(req.session.results.links);
+                            //console.log(req.session.results.links);
                         });
                     })
                 }
