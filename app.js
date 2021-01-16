ccd5407a03d6e378039625aed30696e85e3ba041
Nathan <nathan.fonseca001@gmail.com>
2021-01-15 23:00:53 -0500

    Major update and redesign to backend. Implements Bull for job queues.

--- app.js
+++ app.js
@@ -1,152 +1,356 @@
+// Dependencies
+const fs = require("fs");
 const puppeteer = require("puppeteer");
-
 const express = require("express");
 const app = express();
-var session = require('express-session');
+const session = require('express-session');
 require("dotenv").config();
+const redis = require("redis");
+const redisClient = redis.createClient(process.env.REDIS_URL || `redis://localhost:6379`);
+const RedisStore = require("connect-redis")(session);
 
+// Express setup
 app.set('views', __dirname);
 app.engine('html', require('ejs').__express);
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
+app.use(session({
+    secret: "Shh, its a secret!",
+    store: new RedisStore({client: redisClient}),
+    saveUninitialized: false,
+    resave: false
+}));
 
-app.use(session({secret: "Shh, its a secret!"}));
+// Bull for jobs
+const BullQueue = require("bull");
+const scraperQueue = new BullQueue("scraping", process.env.REDIS_URL || 'redis://localhost:6379');
 
-app.get("/", (req, res) => {
+// Routes
+app.get("/", async (req, res) => {
+    req.session.data = [];
     res.render("index.html");
 })
 
 app.post("/", (req, res) => {
     let keywords = req.body.keyword.split("; ");
-    let options = {
-        resultsCount: req.body.count || 10,
-        website: req.body.website
+    for (let keyword of keywords) {
+        let input = {
+            searchPhrase: keyword,
+            website: req.body.website || '',
+            count: req.body.count || 10,
+            minLength: req.body.minLength || 60
+        }
+
+        scraperQueue.add({...input, sessionId: req.sessionID})
+        .then(job => {
+            req.session.data.push({
+                id: job.id,
+                progress: 0,
+                searchData: input,
+                links: [],
+                results: [],
+                resultsLastIndex: null
+            })
+            req.session.save();
+        })
     }
 
-    req.session.results = {
-        finished: false,
-        links: []
-    };
-    (async () => {
-        await (async () => {
-            for(let keyword of keywords){
-                req.session.results.links.push([])
-                await scrape(keyword, req, options);
-            }
-        })();
-        req.session.results.finished = true;
-        req.session.save();
-    })();
 	res.sendStatus(200);
 })
 
 app.get("/status", (req, res) => {
-    setTimeout(() => {
-        req.session.reload(() => {
-            res.send(req.session.results);
-            if (req.session.results) {
-                req.session.results.links = [[]]
-                req.session.save();
-            }
-        }, 50)
-    })
+    console.log(req.session.data);
 })
 
-let scrape = async (keyword, req, options) => {
+// Scrape job process
+scraperQueue.on('completed', (job, result) => {
+    console.log(result);
+})
+scraperQueue.on('failed', (job, err) => {
+    console.error(err);
+})
 
-    let url = `https://www.google.com/search?q=${keyword}&source=lnms&tbm=vid&num=${options.resultsCount}&as_sitesearch=${options.website}`;
+scraperQueue.process(async (job) => {
+    return getVideoResults(job);
+})
+
+async function getVideoResults(job) {
+    // Open puppeteer and search google videos for keyword
+    let url = `https://www.google.com/search?q=${job.data.searchPhrase}&source=lnms&tbm=vid&num=${job.data.count}&as_sitesearch=${job.data.website}`;
 
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
         ]
     });
-    const page = await browser.newPage();
-    await page.goto(url, {waitUntil: "networkidle2"})
 
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: "networkidle2"});
+
+    // Get all result links and set in session
     await page.waitForSelector("#rso");
     let links = await page.evaluate(() => {
-        let a = [...document.querySelectorAll(".g a")];
-        let aFiltered = a.filter((link, i) => link.classList.length === 0 &&
-            link?.href.includes("google.com/") == false && link?.href != a[i - 1]?.href);
-        return aFiltered.map(a => a?.href);
+        let a = [...document.querySelectorAll(".g a")]; // Selector
+        let aFiltered = a.filter((link, i) => link.classList.length === 0 // Link tag must have no classes
+            && link?.href.includes("google.com/") == false // Link tag url must not include google
+            && link?.href != a[i - 1]?.href); // Link tag url must not be the same as the previous link tag's
+        return aFiltered.map(a => a?.href); // After that filter, the links will be the link tags' hrefs
     });
+    
+    getRedisSessionData(job.data.sessionId)
+    .then(sessData => {
+        sessData.data[getSessionJobIndex(job.id, sessData.data)].links = links;
+        setRedisSessionData(job.data.sessionId, sessData);
+    })
 
-    await (async () => {
-        for(let link of links) {
-            //console.log("searching: " + link);
-            try {
-                await page.goto(link, {waitUntil: "networkidle2", timeout: 20000});
+    for (let link of links) {
+        // Go to link in puppeteer
+        await page.goto(link, {waitUntil: "networkidle2", timeout: 20000});
 
-                let srcArr = await page.evaluate(async (domain) => {
-                    return await new Promise(resolve => {
-                        let url = window.location.href;
-                        if (url.includes("youtube.com/watch")) {
-                            // In the case of youtube, just get an embedded link to use in an iframe
-                            let embeddedLink = "http://youtube.com/embed/" + url.split("?v=")[1];
-                            resolve([embeddedLink]);
+        let url = await page.evaluate(() => {
+            return window.location.href;
+        })
+
+        if (url.includes("youtube.com/watch")) {
+            let linkParams = url.split("?v=")[1];
+            let videoId = linkParams.split("&")[0];
+
+            let embed = `<iframe width="560" height="315" src="${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            let transcript = await getVideoTranscript(page)
+            .catch(e => {
+                console.error(e)
+            });
+            let title = await page.evaluate(() => {
+                return document.querySelector("h1 .style-scope.ytd-video-primary-info-renderer").textContent;
+            })
+            
+            getRedisSessionData(job.data.sessionId)
+            .then(sess => {
+                sess.data[getSessionJobIndex(job.id, sess.data)].results.push({
+                    videoSrc: null,
+                    embed: embed,
+                    url: url,
+                    title: title,
+                    transcript: transcript
+                })
+                setRedisSessionData(job.data.sessionId, sess);
+            })
+        }
+        else { // If the link is not a youtube link
+
+            let siteData = await searchSitesData(job, url, page);
+            if (siteData) {
+                // Add data to session results
+                getRedisSessionData(job.data.sessionId)
+                .then(sess => {
+                    sess.data[getSessionJobIndex(job.id, sess.data)].results.push(siteData);
+                    setRedisSessionData(job.data.sessionId, sess);
+                })
+            }
+            else { // Last resort strategy
+                let title = await page.evaluate(() => {
+                    return document.querySelector("title").textContent;
+                })
+                let dataObj = {
+                    url: url,
+                    title: title,
+                    transcript: null
+                }
+                
+                let iframe = await page.evaluate(() => {
+                    let iframes = document.querySelectorAll("iframe");
+                    let iframe = null;
+                    let iframeWidth = 0;
+                    // Find biggest youtube iframe (youtube embed)
+                    for (let f of iframes) {
+                        if (f.src.includes("youtube.com/") 
+                            && f.getBoundingClientRect().width > iframeWidth) {
+                                iframe = f.outerHTML;
                         }
-                        else if (url.includes(domain)) {
-                            let embeddedLink = 
-                                `https://${domain}.com/embed/${url.split(".com/")[1].split("/")[0]}`;
-                            resolve([embeddedLink]);
-                        }
-                        else if (url.includes("youtube") === false){
-                            // Select video tag and get src (from either video tag, or child source tag)
-                            let videos = document.querySelectorAll("video");
-                            let videoArr = [];
-                            for(let video of videos) {
+                    }
+                    return iframe;
+                })
+
+                if (!iframe) {
+                    // Search for largest video and get src
+                    let src = await page.evaluate(() => {
+                        let videos = document.querySelectorAll("video");
+                        let videoSrc = null;
+                        let videoHeight = 0;
+                        for(let video of videos) {
+                            let measurements = video.getBoundingClientRect();
+                            if (measurements.width >= measurements.height
+                                && measurements.height > videoHeight) {
                                 let srcAtt = video?.querySelector("source")?.getAttribute("src") 
                                     || video?.getAttribute("src");
-                                if (srcAtt) videoArr.push(srcAtt);
+                                if (srcAtt) videoSrc = srcAtt;
                             }
-                            // Check if this a tag exists as well, the video link could be stored in it
-                            let href = document.querySelector(".global-link")?.href;
-                            if (href) videoArr.push(href);
-
-                            resolve(videoArr);
                         }
-                        else resolve([]);
+                        return videoSrc;
                     })
-                }, process.env.DOMAIN1);
-                // Filter src tag to make sure it's not undefined or a blob
-                for(let src of srcArr){
-                    if (src[0] === "/") {
-                        if (src[1] !== "/" && src[1] !== "h") {
-                            let end = ['.com', '.org', '.net'];
-                            for (let i = 0; i < end.length; i++){
-                                if (link.includes(end[i])) {
-                                    let website = link.split(end[i])[0] + end[i];
-                                    src = website + src;
-                                    break;
-                                }
-                            }
-                        }
-                        else if (src[1] === "/") src = "http:" + src;
-                    }
-                    let isBlob = src.includes("blob:") ? true : false;
-                    let isData = src.includes("data:") ? true : false;
-                    if (!isBlob && !isData) {
-                        req.session.reload(() => {
-                            let results = req.session.results.links;
-                            results[results.length-1].push(src); // Add link to last session array
-                            req.session.save(() => {
-                                //console.log(req.session.results.links);
-                            });
-                        })
-                    }
-                }
-            } catch(e) {
-                console.log(e.name + " for: " + link);
-            }
-        };
-    })();
 
-    browser.close();
-    return true;
+                    if (!src) continue;
+                    dataObj.videoSrc = src;
+                    dataObj.embed = null;
+                }
+                else {
+                    dataObj.embed = iframe;
+                    dataObj.videoSrc = null;
+                }
+                
+                getRedisSessionData(job.data.sessionId)
+                .then(sess => {
+                    sess.data[getSessionJobIndex(job.id, sess.data)].results.push(dataObj);
+                    setRedisSessionData(job.data.sessionId, sess);
+                })
+            }
+        }
+    }
+}
+
+async function searchSitesData(job, url, page) {
+    let sites = JSON.parse(fs.readFileSync("siteOptions.json", {encoding: "utf-8"}));
+    // Perform search routines for additional sites based on specific selectors/data
+    for (let site in sites) {
+        if (url.includes(site)) {
+            let dataObj = {
+                url: url,
+                transcript: null
+            };
+
+            // If certain buttons exist which require the video to be active, click them first
+            if (sites[site].conBtn) await page.click(sites[site].conBtn);
+            if (sites[site].startBtn) await page.click(sites[site].startBtn);
+
+            // In case of advertisements, wait for skip button to be active and not blocked first
+            if (sites[site].adBtn) {
+                await page.waitForSelector(sites[site].adBtn, {hidden: true})
+            }
+            if (sites[site].adSkipBtn) {
+                await page.waitForSelector(sites[site].adSkipBtn);
+                await page.click(sites[site].adSkipBtn);
+            }
+
+            // If video source is directly available
+            if (sites[site].videoSelector) {
+                dataObj.videoSrc = await page.evaluate((sel) => {
+                    let src = document.querySelector(sel)?.src;
+                    let href = document.querySelector(sel)?.href;
+                    return src || href;
+                }, sites[site].videoSelector);
+            }
+            // If there is an m3u8 file reference available to retrieve and convert
+            else if (sites[site].file?.m3u8Div) {
+                let m3u8 = await page.evaluate((div) => {
+                    return document.querySelector(div.selector).getAttribute(div.attribute);
+                }, sites[site].file.m3u8Div);
+                console.log("Conversion necessary for: " + m3u8);
+                // TODO: Convert m3u8 to mp4
+            }
+            // If there is a network request to retrieve a video file
+            else if (sites[site].file?.requestName) {
+                await page.setRequestInterception(true);
+                await page.reload();
+
+                page.on("request", async (request) => {
+                    if (request.url().endsWith(sites[site].file.requestName)) {
+                        console.log("Conversion necessary for: " + request.url());
+                        // TODO: Convert to mp4
+                        await page.setRequestInterception(false);
+                    }
+                    request.continue;
+                })
+            }
+
+            dataObj.embed = await page.evaluate((selector, property) => {
+                return document.querySelector(selector)[property];
+            }, sites[site].embedSelector, sites[site].embedProperty);
+
+            dataObj.title = await page.evaluate(() => {
+                return document.querySelector('title').textContent
+            });
+
+            return dataObj;
+        }
+    }
+    return null;
+}
+
+async function getRedisSessionData(sessionId) {
+    let key = 'sess:' + sessionId;
+    return new Promise((resolve, reject) => {
+        redisClient.get(key, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        })
+    })
+    .then(data => {
+        let sessData = JSON.parse(data);
+        return sessData;
+    })
+}
+
+async function setRedisSessionData(sessionId, newDataObj) {
+    let key = 'sess:' + sessionId;
+    redisClient.set(key, JSON.stringify(newDataObj));
+    console.log("SAVING SESSION DATA: ", Date.now());
+}
+
+async function getVideoTranscript(page) {;
+
+    // Check if video is working
+    await page.waitForSelector('.ytp-error', {hidden: true, timeout: 3000});
+
+    // Wait for 'More actions' button to appear
+    await page.waitForSelector("[aria-label='More actions']", {timeout: 5000});
+
+    // Click 'More actions'
+    await page.evaluate(async () => {
+        document.querySelectorAll("[aria-label='More actions']")[1].click();
+    });
+
+    // Wait for 'Open transcript' option to appear
+    await page.waitForSelector(".style-scope.ytd-menu-service-item-renderer", {timeout: 5000});
+
+    // Click 'Open transcript'
+    await page.evaluate(() => {
+        let options = [...document.querySelectorAll(".style-scope.ytd-menu-service-item-renderer")];
+        options.filter(o => o.textContent == "Open transcript")[0].click();
+    })
+
+    await page.waitForSelector(`.cue-group.style-scope.ytd-transcript-body-renderer`);
+
+    // Get all pieces of transcript
+    let transcriptRaw = await page.evaluate(() => {
+        return new Promise(resolve => {
+            let dialogLines = document.querySelectorAll(".cue-group.style-scope.ytd-transcript-body-renderer");
+            let dialogText = [...dialogLines].map(line => {
+                let timestamp = line.querySelector(".cue-group-start-offset.style-scope.ytd-transcript-body-renderer").textContent;
+                let dialogue = line.querySelector(".cue.style-scope.ytd-transcript-body-renderer").textContent;
+                return {timestamp, dialogue};
+            });
+            resolve(dialogText);
+        })
+    })
+
+    let transcript = transcriptRaw.map(tr => {
+        let d = tr.dialogue.replace(/\\n/g, '').trim();
+        let t = tr.timestamp.replace(/\\n/g, '').trim();
+        return {timestamp: t, dialogue: d};
+    });
+    return transcript;
+}
+
+function getSessionJobIndex(id, sessData) {
+    for (let i = 0; i < sessData.length; i++) {
+        if (id === sessData[i].id) {
+            return i;
+        }
+    }
+    return null;
 }
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
